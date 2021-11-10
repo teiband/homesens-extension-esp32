fdbd391c88dee84a68ac5baf18774be1f86b8096
Thomas Eiband <thomas.eiband@dlr.de>
2021-11-10 13:05:18 +0100

    add template for user_defines

---
+++ src/main/README_user_defines.h
@@ -0,0 +1,12 @@
+// copy this file to 'user_defines.h' and adapt the following variables
+// DO NOT commit the copy!!!
+#ifndef USER_DEFINES
+#define USER_DEFINES
+const char* SSID="YOUR_SSID";
+const char* PW="YOUR_PW";
+const char* WS_SERVER_ADDRESS="SOME_IP";
+//unsigned int WS_SERVER_PORT=80;
+unsigned int WS_SERVER_PORT=5000;
+const char* WS_SERVER_URL="/";
+const char* EXTENSION_API_KEY="YOUR_API_KEY";
+#endif
